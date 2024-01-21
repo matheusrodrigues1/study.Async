587a6fbfe824f16ac9bd9f17a7a4aab1fb458bb0
matheusrodrigues1 <matech00@outlook.com>
2024-01-20 23:35:47 -0300

    cadastro de usuatios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+
 
 def cadastro(request):
     if request.method == 'GET':
         return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(
+                request, constants.ERROR, 'As senhas não coincídem'
+            )
+            return redirect('/usuarios/cadastro')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'Já existe um usuário com o mesmo username',
+            )
+            return redirect('/usuarios/cadastro')
+
+        try:
+            user = User.objects.create_user(
+                username=username,
+                password=confirmar_senha,
+            )
+            messages.add_message(
+                request, constants.ERROR, 'Usuário cadastrado com sucesso.'
+            )
+            return redirect('/usuarios/login')
+        except:
+            messages.add_message(
+                request, constants.ERROR, 'Erro interno do sistema'
+            )
+            return redirect('/usuarios/cadastro')
