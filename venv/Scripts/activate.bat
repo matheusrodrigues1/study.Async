870411a52f2e7e70917a09fff89df06602e4f7bf
matheusrodrigues1 <matech00@outlook.com>
2024-02-07 18:56:47 -0300

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\cm613\OneDrive\Área_de_Trabalho\project_python\venv
+set VIRTUAL_ENV=C:\Users\cm613\OneDrive\Área_de_Trabalho\projetos\project_python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
